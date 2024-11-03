d96638c797f2abcbf0854e9fb368b1a9d281b7a3
AlexJohannson <mehedsanya@gmail.com>
2024-11-03 12:04:55 +0100

    [update]:[file]: Task_4; main.js;

---
+++ Lesson_1/Task_4/main.js
@@ -0,0 +1,8 @@
+let firstName = prompt('Enter your firstname', 'write your name');
+console.log(firstName);
+
+let middleName = prompt('Enter your middlename', 'write your middle name');
+console.log(middleName);
+
+let lastName = prompt('Enter your lastname', 'write your lastname');
+console.log(lastName);
