992ba4000598fc4a73dbed25afac7af364ab14c2
AlexJohannson <mehedsanya@gmail.com>
2024-11-04 12:52:35 +0100

    [update]:[file]: Lesson_2 : Task_2  main.js

---
+++ Lesson_2/Arrays_objects/Task_2/main.js
@@ -0,0 +1,20 @@
+let book1 = {
+    title: 'Title',
+    pageCount: 405,
+    genre: 'Thriller'
+}
+console.log(book1);
+
+let book2 = {
+    title: 'Title',
+    pageCount: 276,
+    genre: 'Novel'
+}
+console.log(book2);
+
+let book3 = {
+    title: 'Title',
+    pageCount: 115,
+    genre: 'Fantasy'
+}
+console.log(book3);
