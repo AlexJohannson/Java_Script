477773f3141a83aabc7bf1478c8c9c02f8c4cff0
AlexJohannson <mehedsanya@gmail.com>
2024-11-04 13:35:31 +0100

    [update]:[file]: Lesson_2 : Task_3 main.js

---
+++ Lesson_2/Arrays_objects/Task_3/main.js
@@ -0,0 +1,36 @@
+/*------------------------   Book_1  -----------------------------*/
+
+let book1 = {
+    title: 'Title',
+    pageCount: 36,
+    genre: 'Genre',
+    author: ['Name', 31]
+}
+console.log(book1);
+console.log(book1.genre);
+console.log(book1.author[1]);
+
+
+/*--------------------------   Frontend  ----------------------------*/
+let book2 ={
+    title: 'Frontend',
+    pageCount: 792,
+    genre: 'programming',
+    author: ['Sergii', 37]
+}
+console.log(book2);
+console.log(book2.title);
+console.log(book2.author[0]);
+
+
+
+/*----------------------------   Java Script  --------------------------*/
+let book3 = {
+    title: 'Java Script',
+    pageCount: 653,
+    genre: 'Frontend Developer',
+    author: ['Brendan Eich', 63]
+}
+console.log(book3);
+console.log(book3.pageCount);
+console.log(book3.author);
