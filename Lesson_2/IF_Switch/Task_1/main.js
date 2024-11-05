572ff8bb4d7aa5286884eee7b04e0509f143b0b6
AlexJohannson <mehedsanya@gmail.com>
2024-11-05 12:30:23 +0100

    [update]:[file]: Lesson_2 : IF_Switch: Task_1: main.js

---
+++ Lesson_2/IF_Switch/Task_1/main.js
@@ -0,0 +1,23 @@
+let x = 5;
+
+if(x === 0) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+let a = 1;
+
+if(x === a) {
+    console.log('True');
+} else {
+    console.log('False');
+}
+
+let b = -3;
+
+if(x === b) {
+    console.log('True');
+} else {
+    console.log('False');
+}
