3b56061134686b9a8212e21aa2b5bdedce3e4e62
AlexJohannson <mehedsanya@gmail.com>
2024-11-05 13:33:34 +0100

    [update]:[file]: Lesson_2 : IF_Switch: Task_3: main.js

---
+++ Lesson_2/IF_Switch/Task_3/main.js
@@ -0,0 +1,11 @@
+let day = +prompt('Enter the number from 1 to 31');
+
+if (day <= 11) {
+    confirm('Your number ' + day + ' is in the first decade of the month');
+} else if (day <= 22) {
+    confirm('Your number ' + day + ' is in the second decade of the month');
+} else if (day <= 31) {
+    confirm('Your number ' + day + ' is in the third decade of the month');
+} else {
+    confirm('You entered an incorrect number ' + day + ' a month has a maximum of 31 days');
+}
