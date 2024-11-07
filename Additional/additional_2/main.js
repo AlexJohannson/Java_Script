1f35fc5ee32ac28f00195b0353e81732584670ac
AlexJohannson <mehedsanya@gmail.com>
2024-11-07 16:00:46 +0100

    [update]:[file]: Additional : additional_3: main.js

--- Additional/additional_2/main.js
+++ Additional/additional_2/main.js
@@ -3,26 +3,12 @@ let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
 
 
 
-/*---------------------------   while  ----------------------------*/
-let j =  -1;
-while (j < arr.length){
-    j++;
-    console.log(arr[j]);
-}
-console.log('');
 
-
-/*-----------------  while_back  ----------------------*/
-
-/*--------------------------  odd number -----------------------*/
-let i = -1;
+let i =  -1;
 while (i < arr.length){
-    if (arr[i] % 2 !== 0){
-        console.log(arr[i]);
-    }
     i++;
+    console.log(arr[i]);
 }
-console.log('');
 
 /*------------------ even number  ---------------------------------*/
 
---
+++ Additional/additional_3/main.js
@@ -0,0 +1,10 @@
+let arr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+
+let i = -1;
+while (i < arr.length){
+    if (arr[i] % 2 !== 0){
+        console.log(arr[i]);
+    }
+    i++;
+}
