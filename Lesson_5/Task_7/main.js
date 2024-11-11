81c9f3a66a0acad8788b6db8ed8fe65865733656
AlexJohannson <mehedsanya@gmail.com>
2024-11-11 15:44:35 +0100

    [update]:[file]: Lesson_5: Task_7:index.html, main.js;

---
+++ Lesson_5/Task_7/main.js
@@ -0,0 +1,9 @@
+let list = (text, counter) => {
+    document.write(`<ul>`);
+    for (let i = 0; i < counter; i++) {
+        document.write(`<li>${text}</li>`);
+    }
+    document.write(`</ul>`);
+}
+
+list('JavaScript', 4);
