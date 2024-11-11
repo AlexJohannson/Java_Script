57be34fd477c1dd05743a6780c6bd129dded90cd
AlexJohannson <mehedsanya@gmail.com>
2024-11-11 15:39:00 +0100

    [update]:[file]: Lesson_5: Task_6:index.html, main.js;

---
+++ Lesson_5/Task_6/main.js
@@ -0,0 +1,10 @@
+let word = (text) => {
+    document.write(`
+                 <ul>
+                    <li>${text}</li>
+                    <li>${text}</li>
+                    <li>${text}</li>
+                 </ul>
+                `)
+}
+word('Typescript');
