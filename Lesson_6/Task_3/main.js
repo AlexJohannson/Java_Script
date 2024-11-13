97f95a736f63c1469c9381b90a30504317c5e45f
AlexJohannson <mehedsanya@gmail.com>
2024-11-13 17:15:18 +0100

    [update]:[file]: Lesson_6: Task_3:index.html, main.js;

---
+++ Lesson_6/Task_3/main.js
@@ -0,0 +1,11 @@
+let a = 'HELLO WORLD';
+const aLowerCase = a.toLowerCase();
+console.log(aLowerCase);
+
+let b = 'LOREM IPSUM';
+const bLowerCase = b.toLowerCase();
+console.log(bLowerCase);
+
+let c = 'JAVASCRIPT IS COOL';
+const cLowerCase = c.toLowerCase();
+console.log(cLowerCase);
