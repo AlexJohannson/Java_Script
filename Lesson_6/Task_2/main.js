185f7b46f34e92c62bd032d4a9648e92a4b6d870
AlexJohannson <mehedsanya@gmail.com>
2024-11-13 17:07:12 +0100

    [update]:[file]: Lesson_6: Task_2:index.html, main.js;

---
+++ Lesson_6/Task_2/main.js
@@ -0,0 +1,13 @@
+let a = 'hello world';
+const aUpperCase = a.toUpperCase();
+console.log(aUpperCase);
+
+let b = 'lorem ipsum';
+const bUpperCase = b.toUpperCase();
+console.log(bUpperCase);
+
+let c = 'javascript is cool';
+const cUpperCase = c.toUpperCase()
+console.log(cUpperCase);
+
+
