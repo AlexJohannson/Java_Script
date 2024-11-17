666c3eac11683b67b3bc337ad2e4765a25139796
AlexJohannson <mehedsanya@gmail.com>
2024-11-17 13:11:10 +0100

    [update]:[file]: Lesson_7: Task_8:Task:index.html, main.js;

---
+++ Lesson_7/Task_8/Task/main.js
@@ -0,0 +1,36 @@
+function Cinderella(name, age, footSize) {
+    this.name = name;
+    this.age = age;
+    this.footSize = footSize;
+}
+
+function Prince(name, age, louboutin) {
+    this.name = name;
+    this.age = age;
+    this.louboutin = louboutin;
+}
+
+const cinderellas = [
+    new Cinderella('Viktoria', 28, 30),
+    new Cinderella('Olivia', 27, 28),
+    new Cinderella('Yuliia', 30, 32),
+    new Cinderella('Olha', 33, 34),
+    new Cinderella('Inna', 34, 36),
+    new Cinderella('Svitlana', 29, 31),
+    new Cinderella('Emilia', 35, 37),
+    new Cinderella('Iryna', 35, 38),
+    new Cinderella('Yana', 39, 40),
+    new Cinderella('Olena', 41, 43),
+];
+
+const prince = new Prince('Oleh', 32, 32);
+
+for (const cinderella of cinderellas) {
+    if (cinderella.footSize === prince.louboutin) {
+        console.log(`Prince wife is ${cinderella.name}`);
+    }
+}
+
+const find = cinderellas.find(cinderella => cinderella.footSize === prince.louboutin);
+prince.wife = find;
+console.log(prince.wife);
