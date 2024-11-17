339a60accbc5a1eae2aa82055abf2c59181c1129
AlexJohannson <mehedsanya@gmail.com>
2024-11-17 15:19:24 +0100

    [update]:[file]: Lesson_7: Task_10:index.html, main.js;

---
+++ Lesson_7/Task_10/main.js
@@ -0,0 +1,35 @@
+function User(id, name, surname, email, phone) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+}
+
+let users = [
+    new User(2, 'Oleksandr', 'Oleksandr', 'oleksandr@email.com', '+10000000001'),
+    new User(1, 'Serhii', 'Serhii', 'serhii@email.com', '+20000000002'),
+    new User(3, 'Ivan', 'Ivan', 'ivan@email.com', '+30000000003'),
+    new User(4, 'Dmytro', 'Dmytro', 'dmytro@email.com', '+40000000004'),
+    new User(5, 'Inna', 'Inna', 'inna@email.com', '+50000000005'),
+    new User(6, 'Mykhailo', 'Mykhailo', 'mykhailo@email.com', '+60000000006'),
+    new User(7, 'Viktoria', 'Viktoria', 'viktoria@email.com', '+70000000007'),
+    new User(8, 'Yuliia', 'Yuliia', 'yuliaa@email.com', '+80000000008'),
+    new User(9, 'Valentyna', 'Valentyna', 'valentyna@email.com', '+90000000009'),
+    new User(10, 'Charlotta', 'Charlotta', 'charlotta@email.com', '+1000000000010')
+];
+
+console.log(users);
+
+Array.prototype.myFilter = function (predicat) {
+    const array = [];
+    for (const item of this) {
+        if (predicat(item)) {
+            array.push(item);
+        }
+    }
+    return array;
+};
+
+const filter = users.myFilter(user => user.id > 6);
+console. log(filter);
