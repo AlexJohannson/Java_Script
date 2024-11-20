b025e086f5a124c999a0a22df6d568807c90a89b
AlexJohannson <mehedsanya@gmail.com>
2024-11-20 17:06:23 +0100

    [update]:[file]: Lesson_9: Task_1:index.html, style.css, main.js;

--- Lesson_9/Task_1/main.js
+++ Lesson_9/Task_1/main.js
@@ -4,7 +4,7 @@ let text = document.createElement('h1')
 text.innerText = 'HTML / CSS';
 block1.appendChild(text);
 const cloneOfNode1 = block1.cloneNode(true);
-document.body.append(cloneOfNode1);
+document.body.append(block1, cloneOfNode1);
 
 const block2 = document.createElement('div');
 block2.classList.add('collapse');
@@ -12,7 +12,7 @@ let text2 = document.createElement('h1');
 text2.innerText = 'JavaScript';
 block2.appendChild(text2);
 const cloneOfNode2 = block2.cloneNode(true);
-document.body.append(cloneOfNode2);
+document.body.append(block2, cloneOfNode2);
 
 const block3 = document.createElement('div');
 block3.classList.add('alpha');
@@ -20,7 +20,7 @@ let text3 = document.createElement('h1');
 text3.innerText = 'React';
 block3.appendChild(text3);
 const cloneOfNode3 = block3.cloneNode(true);
-document.body.append(cloneOfNode3);
+document.body.append(block3, cloneOfNode3);
 
 const block4 = document.createElement('div');
 block4.classList.add('beta');
@@ -28,7 +28,12 @@ let text4 = document.createElement('h1');
 text4.innerText = 'Node.js';
 block4.appendChild(text4);
 const cloneOfNode4 = block4.cloneNode(true);
-document.body.append(cloneOfNode4);
+document.body.append(block4, cloneOfNode4);
+console.log(cloneOfNode4);
+console.log(block4);
+console.log(block4.childNodes);
+console.log(document.querySelectorAll('div'));
+console.log(document.querySelectorAll('h1'));
 
 
 
