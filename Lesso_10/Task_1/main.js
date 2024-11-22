6adc6fb4cb83e13bdd04043e14ec38a870304c1b
AlexJohannson <mehedsanya@gmail.com>
2024-11-22 14:54:48 +0100

    [update]:[file]: Lesson_10/ Task_1/index.html, style.css, main.js;

---
+++ Lesso_10/Task_1/main.js
@@ -0,0 +1,18 @@
+
+const buttonPress = document.getElementsByClassName('Press')[0];
+const quote = document.getElementById('quote');
+const quoteTitle = quote.querySelector('h2');
+const quoteText = quote.querySelector('p');
+
+let click = 1;
+
+buttonPress.onclick = function (){
+    if (click === 1) {
+        quoteTitle.style.display = 'none';
+    } else if (click === 2) {
+        quoteText.style.display = 'none';
+    } else if (click === 3) {
+        quote.style.display = 'none';
+    }
+    click++;
+}
