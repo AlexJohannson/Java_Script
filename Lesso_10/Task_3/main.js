66dba240f1ee6667f1ca431687f3550c5a73630e
AlexJohannson <mehedsanya@gmail.com>
2024-11-22 20:17:16 +0100

    [update]:[file]: Lesson_10/ Task_3/index.html, style.css, main.js;

---
+++ Lesso_10/Task_3/main.js
@@ -0,0 +1,23 @@
+const form  = document.getElementById('form');
+const inputName = document.getElementById('inputName');
+const inputSurname = document.getElementById('inputSurname');
+const inputAge = document.getElementById('inputAge');
+const nameObject = document.getElementById('nameObject');
+const nameObjectTitle = document.getElementsByTagName('h1')[0];
+
+
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const nameValue = inputName.value;
+    const surnameValue = inputSurname.value;
+    const ageValue = inputAge.value;
+    let personObject = {nameValue, surnameValue, ageValue};
+    console.log(personObject);
+    nameObjectTitle.innerText = `${personObject.nameValue} ${personObject.surnameValue} ${personObject.ageValue}`;
+    if (nameObjectTitle){
+        nameObject.style.background = 'lightgray';
+    }
+
+})
