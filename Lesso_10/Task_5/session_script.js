3643c9bfad30a4a09b6b949ab346940f9e6b87b1
AlexJohannson <mehedsanya@gmail.com>
2024-11-23 13:13:40 +0100

    [update]:[file]: Lesson_10/ Task_5/index.html, main.js, session.html, style.css, session_script

--- Lesso_10/Task_5/session_script.js
+++ Lesso_10/Task_5/session_script.js
@@ -3,6 +3,8 @@ const sessionList = document.getElementById('sessionList');
 let session = JSON.parse(localStorage.getItem('session'));
 for (const sessionTime of session) {
     const sessionDate = document.createElement('h3');
+    sessionDate.style.background = 'lightgray';
+    sessionDate.style.paddingBottom = '10px'
     sessionDate.innerText = sessionTime.toString();
     sessionList.appendChild(sessionDate);
 }
