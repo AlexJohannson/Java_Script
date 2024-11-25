e428eaddacfbd4fec5813c02ed6204b4e164edb2
AlexJohannson <mehedsanya@gmail.com>
2024-11-25 21:47:51 +0100

    [update]:[file]:Lesson_11/Task_1/index.html, main.js

--- Lesson_11/Task_1/main.js
+++ Lesson_11/Task_1/main.js
@@ -1,33 +1,31 @@
 const catalog = document.getElementById('catalog');
 
 fetch('https://dummyjson.com/carts')
-      .then(values =>values.json())
-      .then(objectCards =>{
-          const {carts} = objectCards;
-          console.log(carts);
+    .then(values => values.json())
+    .then(objectCards => {
+        const {carts} = objectCards;
+        console.log(carts);
 
-          for (const card of carts) {
-              const blockCard = document.createElement('div');
-              blockCard.classList.add('blockCard');
-              const informationCard = document.createElement('p');
-              informationCard.innerText =`
+        for (const card of carts) {
+            const blockCard = document.createElement('div');
+            blockCard.classList.add('blockCard');
+            const informationCard = document.createElement('p');
+            informationCard.innerText = `
               total: ${card.total};
               discountedTotal: ${card.discountedTotal};
               userId: ${card.userId};
               totalProducts: ${card.totalProducts};
               totalQuantity: ${card.totalQuantity};
               `
-              blockCard.appendChild(informationCard);
+            blockCard.appendChild(informationCard);
 
 
-
-
-              const olListItem = document.createElement('ol');
-              olListItem.classList.add('olListItem');
-              for (const product of card.products) {
-                  const liListItem = document.createElement('li');
-                  liListItem.classList.add('liListItem');
-                  liListItem.innerText = `
+            const olListItem = document.createElement('ol');
+            olListItem.classList.add('olListItem');
+            for (const product of card.products) {
+                const liListItem = document.createElement('li');
+                liListItem.classList.add('liListItem');
+                liListItem.innerText = `
                   Id: ${product.id};
                   Title: ${product.title};
                   Price: ${product.price};
@@ -37,14 +35,14 @@ fetch('https://dummyjson.com/carts')
                   Discount total: ${product.discountedTotal};
                   `
 
-                  const image = document.createElement('img');
-                  image.src = product.thumbnail;
+                const image = document.createElement('img');
+                image.src = product.thumbnail;
 
-                  olListItem.appendChild(liListItem);
-                  liListItem.appendChild(image);
-              }
+                olListItem.appendChild(liListItem);
+                liListItem.appendChild(image);
+            }
 
-              blockCard.appendChild(olListItem);
-              catalog.appendChild(blockCard);
-          }
-      });
+            blockCard.appendChild(olListItem);
+            catalog.appendChild(blockCard);
+        }
+    });
