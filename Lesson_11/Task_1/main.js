44470612daae7b2c776fd31dd4adbe142cb65bb8
AlexJohannson <mehedsanya@gmail.com>
2024-11-25 15:46:13 +0100

    [update]:[file]:Lesson_11/Task_1/index.html, main.js

---
+++ Lesson_11/Task_1/main.js
@@ -0,0 +1,44 @@
+const catalog = document.getElementById('catalog');
+
+fetch('https://dummyjson.com/carts')
+      .then(values =>values.json())
+      .then(objectCards =>{
+          const {carts} = objectCards;
+          console.log(carts);
+
+          for (const card of carts) {
+              const blockCard = document.createElement('div');
+              blockCard.classList.add('blockCard');
+              const informationCard = document.createElement('p');
+              informationCard.innerText =`
+              total: ${card.total};
+              discountedTotal: ${card.discountedTotal};
+              userId: ${card.userId};
+              totalProducts: ${card.totalProducts};
+              totalQuantity: ${card.totalQuantity};
+              `
+
+              const olListItem = document.createElement('ol');
+              olListItem.classList.add('olListItem');
+              for (const product of card.products) {
+                  const liListItem = document.createElement('li');
+                  liListItem.classList.add('liListItem');
+                  liListItem.innerText = `
+                  Id: ${product.id};
+                  Title: ${product.title};
+                  Price: ${product.price};
+                  Quantity: ${product.quantity};
+                  Total: ${product.total};
+                  Discount: ${product.discountPercentage};
+                  Discount total: ${product.discountedTotal};
+                  `
+                  const image = document.createElement('img');
+                  image.src = product.thumbnail;
+
+                  liListItem.appendChild(image);
+                  olListItem.appendChild(liListItem);
+              }
+              blockCard.appendChild(olListItem);
+              catalog.appendChild(blockCard);
+          }
+      });
