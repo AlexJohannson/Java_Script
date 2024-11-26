dff7b262a633c1e827f5b9f3b632e27c46f2a520
AlexJohannson <mehedsanya@gmail.com>
2024-11-26 10:20:09 +0100

    [update]:[file]:Lesson_11/Task_2/index.html, main.js

---
+++ Lesson_11/Task_2/main.js
@@ -0,0 +1,31 @@
+const container = document.getElementById('container');
+
+fetch('https://dummyjson.com/recipes')
+    .then(response => response.json())
+    .then(data => {
+        const recipes = data.recipes;
+        console.log(recipes);
+        recipes.forEach(recipe => {
+            const recipeBlock = document.createElement('div');
+            recipeBlock.classList.add('recipe');
+
+            const recipeTitle = document.createElement('h2');
+            recipeTitle.innerText = recipe.name;
+            recipeBlock.appendChild(recipeTitle);
+
+            const recipeImage = document.createElement('img');
+            recipeImage.src = recipe.image;
+            recipeBlock.appendChild(recipeImage);
+
+            const ingredientsUlList = document.createElement('ul');
+            ingredientsUlList.classList.add('ingredients');
+            recipe.ingredients.forEach(ingredient => {
+                const ingredientLiItem = document.createElement('li');
+                ingredientLiItem.innerText = ingredient;
+                ingredientsUlList.appendChild(ingredientLiItem);
+            });
+            recipeBlock.appendChild(ingredientsUlList);
+
+            container.appendChild(recipeBlock);
+        });
+    })
